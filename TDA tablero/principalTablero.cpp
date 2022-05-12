96e04a0f5378b8a208724534e3983fc2fbe668c3
Adán <84880835+adandemarco@users.noreply.github.com>
2022-05-12 12:45:56 -0300

    Create principalTablero.cpp

---
+++ TDA tablero/principalTablero.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "tablero.h"
+
+using namespace std;
+
+
+int main() {
+	
+	tablero *miTablero = new tablero(5, 3, 3);
+	
+	miTablero->cargarFilas(5);
+	miTablero->cargarColumnas(3);
+	miTablero->cargarProfundidad(3);
+	//miTablero->generarMatriz();
+	miTablero->rellenarMatriz();
+	miTablero->mostrarTablero();
+	cout<<"\nmodificar posición \n";
+	miTablero->modificarPosicion(1, 4, 2, 2); // el primer valor sería el jugador después x, y, z
+	miTablero->mostrarTablero();
+	
+	return 0;
+}
