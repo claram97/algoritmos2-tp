314720993deae606d592a25d96afa334d31e7d86
claram97 <102437348+claram97@users.noreply.github.com>
2022-05-27 01:19:01 -0300

    Update jugador.h

--- TDA Jugador/jugador.h
+++ TDA Jugador/jugador.h
@@ -9,20 +9,35 @@ private:
 	unsigned int id;
 	Ficha** fichas;
 	unsigned int cantidadDeFichas;
-	std::deque<int *>* cartas; //TODO: Reemplazar int por TDA cartas
+	std::deque<int**> cartas; //TODO: Reemplazar int por TDA cartas
 
 public:
+	//Pre: -
+	//Post: crea un jugador con su respectivo id, la cantidad de fichas que posee y las cartas en su mazo.
 	Jugador(int id, int cantidadDeFichas, std::deque<int*>* cartas);
 	
+	//Pre: -
+	//Post: devuelve la cantidad de fichas.
 	unsigned int getCantidadDeFichas();
 	
     	//Pre: recibe una cantidad de fichas mayor a 0.
     	//Post: inicializa la cantidad de fichas.
     	void setCantidadDeFichas(unsigned int cantidadDeFichas);
 	
+	//Pre: -
+	//Post: devuelve la cantidad de cartas.
 	unsigned int getCantidadDeCartas();
+	
+	//Pre: recibe una carta.
+	//Post: de ser posible, agrega la carta al mazo.
 	void agregarCarta(int*carta); //cambiar por Cartas*
+	
+	//Pre: (habria que poner alguna condicion, tipo el tipo de carta o algo)
+	//Post: devuelve una carta.
 	int* getCarta(); //cambiar por Cartas*
+	
+	//Pre: -
+	//Post: disminuye en 1 la cantidad de fichas.
 	void decrementarCantidadDeFichas();
 
 };
