904db091b0e3b28449f10b433954f2594a810a75
claram97 <102437348+claram97@users.noreply.github.com>
2022-05-31 11:52:21 -0300

    Update BatallaCampal.h

--- Batalla Campal/BatallaCampal.h
+++ Batalla Campal/BatallaCampal.h
@@ -16,7 +16,7 @@ class BatallaCampal{
 	
 	private:
 		Vector <Jugador*>* jugadores;
-		unsigned int turno; //le podemos poner jugador actual? xD
+		unsigned int turno;
 		Tablero* tablero;
 		int dimensionTablero;
 		int cantidadJugadores;
