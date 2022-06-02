a58fc05e6497e3a4d2c9293651c9de0145c00bae
LucasVCorrea <92963867+LucasVCorrea@users.noreply.github.com>
2022-06-02 11:32:30 -0300

    Update Aleatorio.h

--- Batalla Campal/Aleatorio.h
+++ Batalla Campal/Aleatorio.h
@@ -26,8 +26,38 @@ class Coordenadas{
 		void setCoordenadaX(int);
 		void setCoordenadaY(int);
 		void setCoordenadaZ(int);
+
+
+/*
+	Comentario de lucas C
+	--- > yo lo pensaria mas por la opcion que voy a poner debajo, habria que ponerlo en el ultimo for de la matriz cubica nomas
+*/
+	
+void random(){
+    const int dimension = 10;
+    int tablero[dimension][dimension]; 
+    
+    
+    for (int p=0;p<10;p++)
+        {cout<<"["<<p<<"]";}
+    cout<<endl;
+    
+    for (int i=0;i<dimension;i++){
+        cout<<"["<<i<<"] ";
+        
+		for(int j=0;j<dimension;j++){
+           
+            if(tablero[i][j] % 2 ==0){  // aca se puede poner tambien que tablero[i][j][k] == PISO y va a hacer los cambios solo en el piso
+                tablero[i][j] = rand()%5; //va a poner numeros random por todo el lugar de 1 a 5, 5 seria reemplazado por numero de jugadores +1 
+            }
+            else{
+                tablero[i][j]= 9 ; // el 9 es una posicion vacia, osea donde no hay ningun jugador
+            }
+            cout<<tablero[i][j]<<"  "; 
+            }
+        cout<<"\n"; 
+        }
+
 }
-
-
-
+}
 #endif /* SRC_ALEATORIO_H_ */
