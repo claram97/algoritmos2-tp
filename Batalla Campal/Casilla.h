cd8362e32841604043f2e0f8119f78cc149f8d6c
Lucas-Jun <75916652+Lucas-Jun@users.noreply.github.com>
2022-06-06 00:07:06 -0300

    Update Casilla.h

--- Batalla Campal/Casilla.h
+++ Batalla Campal/Casilla.h
@@ -34,60 +34,60 @@ class Casilla{
 		
 	public:
 
-		void setTipo(TipoDeCasillero tipo);
-
 		/*//Pre: recibe las coordenadas del tablero.
 		//Post: devuelve true si las proporciones del tablero son vÃ¡lidas.
 		//(Me parece que esto es mÃ¡s para tablero pero bueno ya lo tenÃ­a hecho asÃ­ que si querÃ©s y si te sirve adaptalo)
 		//(Y sino cambiame las cosas que creas necesarias)
 		bool proporcionesValidasTablero(unsigned int largo, unsigned int ancho, unsigned int alto);
-	*/
+		*/
 
-
-		//Pre: recibe las coordenadas del tablero y una cantidad de tokens (fichas).
-		//Post: crea una casilla.
+		/*
+		 * Pre: Recibe las coordenadas del tablero y una cantidad de tokens (fichas)
+		 * Pos: Crea una casilla
+		 */
 		Casilla(unsigned int coordenadaX, unsigned int coordenadaY, unsigned int coordenadaZ, TipoDeCasillero tipo);
-	
-
-		//Pre: recibe un tipo para la casilla (mar, tierra o aire).
-		//Post: inicializa el tipo de la casilla segÃºn su tipo.
+		/*
+		 * Pre: Recibe un tipo para la casilla (mar, tierra o aire)
+		 * Pos: Inicializa el tipo de la casilla segÃºn su tipo
+		 */
 		void setTipoDeCasilla(TipoDeCasillero tipoDeCasilla);
-
-		//Pre:
-		//Post: devuelve el tipo de la casilla.
+		/*
+		 * Pos: Devuelve el tipo de casilla
+		 */
 		TipoDeCasillero getTipoDeCasilla();
-	
-		//Pre:
-		//Post: devuelve la coordenada X de la casilla.
+		/*
+		 * Pos: Devuelve la coordenada X/fila de la casilla
+		 */
 		unsigned int getCoordenadaX();
-
-
-	
-		//Pre:
-		//Post: devuelve la coordenada Y de la casilla.
+		/*
+		 * Pos: Devuelve la coordenada Y/columna de la casilla
+		 */
 		unsigned int getCoordenadaY();
-
-		
-
-		//Pre:
-		//Post: devuelve la coordenada Z de la casilla.
+		/*
+		 * Pos: Devuelve la coordenada Z/altura de la casilla
+		 */
 		unsigned int getCoordenadaZ();
-		
+		/*
+		 * Pos: Devuelve el estado del casillero
+		 */
 		EstadoDelCasillero getEstado();
-		
+		/*
+		/* Pre: Necesita un tipo de estado
+		 * Pos: Setea el estado del casillero al tipo usado
+		 */	
 		void setEstado(EstadoDelCasillero estado);
-
-		//pre:
-		//post:
+		/*
+		 * Pos: Devuelve el contenido del casillero
+		 */
 		TipoDeCasillero getContenido();
-
-		//pre:
-		//post:
+		/*
+		/* Pre: Necesita un tipo de casilla
+		 * Pos: Setea el tipo del casillero al tipo usado
+		 */	
 		void setTipoDeCasillero(TipoDeCasillero tipo);
-
-		//pre:
-		//post:
+		/*
+		 * Pos: Setea las coordenadas al casillero si estas son válidas 
+		 */
 		void setCoordenadas ();
-
 };
 #endif
