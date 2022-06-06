4e10cfef297e07f655de03f3a7c6019ac8ee5411
Lucas-Jun <75916652+Lucas-Jun@users.noreply.github.com>
2022-06-06 20:23:07 -0300

    Update Casilla.h

--- Batalla Campal v.2/Casilla.h
+++ Batalla Campal v.2/Casilla.h
@@ -24,49 +24,56 @@ class Casilla{
 		EstadoDelCasillero estadoCasilla;
 		TipoDeCasillero tipoDeCasilla;
 
+
+
 	public:
+
 		/*
 		 * Pre: Recibe las coordenadas del tablero y una cantidad de tokens (fichas)
-		 * Post: Crea una instancia de casilla
+		 * Pos: Crea una casilla
 		 */
 		Casilla(unsigned int coordenadaX, unsigned int coordenadaY, unsigned int coordenadaZ, TipoDeCasillero tipo);
+
 		/*
 		 *Pre: recibe una coordenada que se quiere inicializar y el valor al cual se lo quiere inicializar.
 		 *Post: inicializa la coordenada.
 		 */
 		void setCoordenada(unsigned int &coordenadaAModificar, unsigned int coordenada);
+		
 		/*
 		 * Pre: Recibe un tipo para la casilla (mar, tierra o aire)
-		 * Post: Inicializa el tipo de la casilla segÃºn su tipo
+		 * Pos: Inicializa el tipo de la casilla segÃºn su tipo
 		 */
 		void setTipoDeCasillero(TipoDeCasillero tipoDeCasilla);
 		/*
-		 * Post: Devuelve el tipo de casilla
-		 */
+		* Pos: Devuelve el tipo de casilla
+		*/
 		TipoDeCasillero getTipoDeCasilla();
 		/*
-		 * Post: Devuelve la coordenada X/fila de la casilla
+		 * Pos: Devuelve la coordenada X/fila de la casilla
 		 */
 		unsigned int getCoordenadaX();
+
 		/*
-		 * Post: Devuelve la coordenada Y/columna de la casilla
+		 * Pos: Devuelve la coordenada Y/columna de la casilla
 		 */
 		unsigned int getCoordenadaY();
+
 		/*
-		 * Post: Devuelve la coordenada Z/altura de la casilla
+		 * Pos: Devuelve la coordenada Z/altura de la casilla
 		 */
 		unsigned int getCoordenadaZ();
 		/*
-		 * Post: Devuelve el estado del casillero
+		 * Pos: Devuelve el estado del casillero
 		 */
 		EstadoDelCasillero getEstado();
 		/*
 		 * Pre: Necesita un tipo de estado
-		 * Post: Setea el estado del casillero al tipo usado
+		 * Pos: Setea el estado del casillero al tipo usado
 		 */
 		void setEstado(EstadoDelCasillero estado);
 		/*
-		 * Post: Setea las coordenadas al casillero si estas son válidas
+		 * Pos: Setea las coordenadas al casillero si estas son válidas
 		 */
 		void setCoordenadas ();
 
