f755029e632ec39bd9bac3e9166e209b1c5f3b0a
Lucas-Jun <75916652+Lucas-Jun@users.noreply.github.com>
2022-06-06 15:09:58 -0300

    Update Ficha.h

--- Batalla Campal v.2/Ficha.h
+++ Batalla Campal v.2/Ficha.h
@@ -32,65 +32,59 @@ private:
 public:
 		
 	/*
-	 *  Pre:
-	 *  Post: crea una ficha con el tipo sin definir y posiciones seteadas en -1 (deben cambiarse luego).
+	 *  Post: Crea una ficha con el tipo sin definir y posiciones seteadas en -1 (deben cambiarse luego).
 	 */
 	Ficha();
-
 	/*
 	 * Pre: Recibe un tipo de ficha, con valores válidos: SOLDADO, AVIÓN, BARCO, MINAS.
 	 * Post: Se crea una ficha del tipo dado si el valor es válido, sino deja el tipo sin definir.
 	 * Las posiciones iniciales estarán seteadas en -1, deben actualizarse.
-	*/
+	 */
 	Ficha(char tipo, int x, int y, int z);
-		
 	/*
-	 *  Pos: Devuelve la posicion X/fila
+	 *  Post: Devuelve la posicion X/fila
 	 */
 	int getPosicionY();
-	
 	/*
-	 *  Pos: Devuelve la posicion Y/columna
+	 *  Post: Devuelve la posicion Y/columna
 	 */
 	int getPosicionX();
-	
 	/*
-	 *  Pos: Devuelve la posicion Z/altura
+	 *  Post: Devuelve la posicion Z/altura
 	 */
 	int getPosicionZ();
 	/*
-	 * pre: -
-	 * pos: devuelve el tipo de ficha
-	*/
+	 *	Post: Devuelve el tipo de ficha
+	 */
 	char getTipo();
 	/*
-	 * pre: el tipo tiene que ser soldado, avion, barco o mina.
-	 * post: setea el tipo de la ficha actual según el tipo recibido.
-	*/
+	 * Pre: El tipo tiene que ser soldado, avion, barco o mina.
+	 * Post: Setea el tipo de la ficha actual según el tipo recibido.
+	 */
 	void setTipo(char tipo);
 	/*
 	 *  Pre: Necesita las coordenadas de la fila y la columna
-	 *  Pos: Si es soldado, cambia su posicion
+	 *  Post: Si es soldado, cambia su posicion
 	 */
 	void setCoordenadasSoldado(int posicionX, int posicionY);
 	/*
 	 *  Pre: Necesita las coordenadas de la casilla
-	 *  Pos: Devuelve true si la casilla es valida
+	 *  Post: Devuelve true si la casilla es valida
 	 */
 	bool esPosicionValida(int x, int y, int z);
 	/*
 	 *  Pre: Necesita un tipo de ficha
-	 *  Pos: Devuelve true si el tipo de ficha es valido
+	 *  Post: Devuelve true si el tipo de ficha es valido
 	 */
 	bool esTipoValido(char tipo);
 	/*
 	*  Pre: Necesita las coordenadas y un tipo de ficha
-	*  Pos:
+	*  Post: Devuelve true si el tipo de ficha es valido para el tipo de casilla
 	*/
 	bool tipoConcuerdaConPosicion(int x, int y, int z, char tipo);
 	/*
 	 *  Pre: Necesita un tipo de ficha
-	 *  Pos: Setea la ficha al tipo dado
+	 *  Post: Setea la ficha al tipo dado
 	 */
 	void definirTipo(char tipo);
 };
