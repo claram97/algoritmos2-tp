cc36f80b133a811e19c7d58528dcff7c17059c08
Lucas-Jun <75916652+Lucas-Jun@users.noreply.github.com>
2022-06-10 16:30:41 -0300

    Update crearTablero.cpp

--- Bitmap/crearTablero.cpp
+++ Bitmap/crearTablero.cpp
@@ -5,7 +5,44 @@
 
 using namespace std;
 
-int creacionTablero( int tamanio){
+int creacionTablero(BMP &Image, int tamanio){
+	RGBApixel LightGray;
+	LightGray.Red = 192;
+	LightGray.Green = 192;
+	LightGray.Blue = 192;
+	for( int j=0 ; j < Image.TellHeight() ; j++ )
+	{
+	for( int i=0 ; i < Image.TellWidth() ; i++ )
+	{
+	*Image(i,j) = LightGray;
+	}
+	}
+	RGBApixel FontColor; 
+	FontColor.Red = 0;
+	FontColor.Green = 0;
+	FontColor.Blue = 0; 
+	for (int i = 20; i <= tamanio*tamanio; i = i+20){
+		for (int j = 0; j <= tamanio*tamanio; j = j+20){
+			DrawLine(Image, i, j, i, 400, FontColor);
+			DrawLine(Image, j, i, 400, i, FontColor);
+		}
+	}
+	DrawArc(Image, 10, 10, 5, 0, 360, FontColor);
+	Image.WriteToFile( "tablero.bmp" );
+	return 0;
+}
+int pintarCuadrado(BMP &Image, int xi,int yi, int xf, int yf){
+	RGBApixel FontColor; 
+	FontColor.Red = 255;
+	FontColor.Green = 0;
+	FontColor.Blue = 0; 
+	for (yi; yi <= yf ; yi++){
+		DrawLine(Image, xi, yi, xf, yi, FontColor);
+	}
+	Image.WriteToFile( "tablero.bmp" );	
+}
+
+int main(){
 	RGBApixel LightGray;
 	LightGray.Red = 192;
 	LightGray.Green = 192;
@@ -19,16 +56,7 @@ int creacionTablero( int tamanio){
 	*Window(i,j) = LightGray;
 	}
 	}
-	RGBApixel FontColor; 
-	FontColor.Red = (ebmpBYTE) ebmpRound( (double) 3 * 255.0 / (double) Window.TellHeight() );
-	FontColor.Green = 0;
-	FontColor.Blue = 255 - FontColor.Red; 
-	for (int i = 20; i <= tamanio*tamanio; i = i+20){
-		for (int j = 0; j <= tamanio*tamanio; j = j+20){
-			DrawLine(Window, i, j, i, 400, FontColor);
-			DrawLine(Window, j, i, 400, i, FontColor);
-		}
-	}
-	Window.WriteToFile( "tablero.bmp" );
-	return 0;
-}
+	creacionTablero(Window, 20);
+	
+	pintarCuadrado(Window, 40, 0, 60, 20);
+	return
